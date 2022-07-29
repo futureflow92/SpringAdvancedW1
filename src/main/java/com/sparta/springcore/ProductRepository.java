a23c77b8a46294ca4621e14e8369ff9a29114f89
futureflow92 <futureflow92@hotmail.com>
2022-07-29 18:16:07 +0900

    패키지 생성하여 나눔 (model, dto, controller, repository, service)

--- src/main/java/com/sparta/springcore/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.springcore;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> { }
--- src/main/java/com/sparta/springcore/controller/ProductController.java
+++ src/main/java/com/sparta/springcore/controller/ProductController.java
@@ -1,19 +1,24 @@
-package com.sparta.springcore;
+package com.sparta.springcore.controller;
 
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
+
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController() {
-        ProductService productService = new ProductService();
+    @Autowired
+    public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
